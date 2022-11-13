751634753d9e6ae0e17f44e5cdb8a7490b6f3f55
ranjith <ranjithstar256@gmail.com>
2022-11-13 18:57:02 +0530

    Initial commit

---
+++ app/src/main/java/ran/am/androidmanifestercodeskotlin/MainActivity.kt
@@ -0,0 +1,24 @@
+package ran.am.androidmanifestercodeskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf(
+            "Virat Kohli", "Rohit Sharma", "Steve Smith",
+            "Kane Williamson", "Ross Taylor"
+        )
+
+        // access the listView from xml file
+        var mListView = findViewById<ListView>(R.id.idlistv)
+        arrayAdapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, users)
+        mListView.adapter = arrayAdapter
+    }
+}
